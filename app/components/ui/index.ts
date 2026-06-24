67a4949ac20aed3535b2e64e85726a30109394d7
Jeeva-F22 <jeeva@f22labs.com>
2026-06-24 13:33:29 +0530

    feat: add barrel index for UI components (design-sync named exports)

---
+++ app/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as CountUp } from './CountUp';
+export { default as Magnetic } from './Magnetic';
+export { default as Marquee } from './Marquee';
+export { default as SectionHeading } from './SectionHeading';
+export { default as SubtleBackground } from './SubtleBackground';
